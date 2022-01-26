0607a14a5e93754535047ff0602e4da5f9afcb77
Jul-Me <memai.julian@gmail.com>
2022-01-26 22:47:07 +0000

    feat: installed react-boostrap and used Styled components for nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,8 +1,9 @@
-import classNames from 'classnames';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import StyledNav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 import { NavItems } from '../../types/NavItems';
-import './Nav.scss';
 
 function Nav() {
   const navItems: NavItems[] = [
@@ -13,20 +14,33 @@ function Nav() {
     { id: 'contact', text: 'Contact', url: '#contact', isButton: true },
   ];
 
-  return (
-    <nav className="nav">
-      <div className="nav__logo"> Logo </div>
+  const renderItem = (item: NavItems) => {
+    if (!item.isButton) {
+      return (
+        <StyledNav.Link key={item.id} href={item.url}>
+          {item.text}
+        </StyledNav.Link>
+      );
+    } else {
+      return (
+        <Link key={item.id} to={item.url} className="button ms-md-3 m-auto">
+          {item.text}
+        </Link>
+      );
+    }
+  };
 
-      <ul className="nav__items">
-        {navItems?.map((item) => (
-          <li key={item.id}>
-            <Link to={item.url} className={classNames('nav__link', { button: item.isButton })}>
-              {item.text}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Navbar.Collapse id="basic-navbar-nav">
+          <StyledNav className="ms-auto align-items-center">{navItems?.map(renderItem)}</StyledNav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
