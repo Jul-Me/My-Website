1871aa623f6816a5c97a4e3b0eeb6e375a17d5a1
Jul-Me <memai.julian@gmail.com>
2022-01-28 00:34:01 +0000

    feat: Added gutters, better responsive cols and timeouted animation

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,35 +1,45 @@
-import React from 'react';
+import React, { SyntheticEvent, useState } from 'react';
+import { v4 as uuid } from 'uuid';
 import { Col, Row, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithubSquare, faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
 import './Hero.scss';
 
 function Hero() {
-  const renderGreeting = () => (
-    <div className="hero-text">
-      <h3>Hi there, I'm</h3>
-      <h1 className="display-2">Julian Memai</h1>
-      <h3>Junior Software Developer</h3>
+  const transformation = (str: string) => [...str].map((char) => <Item key={uuid()} char={char} />);
 
-      <button className="button mt-4">Contact</button>
-    </div>
-  );
+  const renderGreeting = () => {
+    const str1 = `Hi there, I'm`;
+    const str2 = `Julian Memai`;
+    const str3 = `Junior Software Developer`;
+
+    return (
+      <div className="hero-text">
+        <h3>{transformation(str1)}</h3>
+        <h1 className="display-2">{transformation(str2)}</h1>
+        <h3>{transformation(str3)}</h3>
+
+        <button className="button mt-4">Contact</button>
+      </div>
+    );
+  };
 
   const renderIcons = () => (
     <Row className="hero-icons align-items-center">
-      <Col md={12} xs={4}>
+      <Col lg={12} xs={4}>
         <FontAwesomeIcon icon={faLinkedin} size="lg" />
       </Col>
 
-      <Col md={12} xs={4}>
+      <Col lg={12} xs={4}>
         <FontAwesomeIcon icon={faGithubSquare} size="lg" />
       </Col>
 
-      <Col md={12} xs={4}>
+      <Col lg={12} xs={4}>
         <FontAwesomeIcon icon={faTwitterSquare} size="lg" />
       </Col>
     </Row>
   );
+
   return (
     <div className="hero">
       <Container>
@@ -38,13 +48,11 @@ function Hero() {
             {renderGreeting()}
           </Col>
 
-          <Col xl={5} lg={6} md={11} className="gy-5">
+          <Col xl={5} lg={6} md={12} className="gy-5">
             <div className="hero-img"></div>
           </Col>
 
-          <Col md={1} className="gy-5">
-            {renderIcons()}
-          </Col>
+          <Col className="gy-5">{renderIcons()}</Col>
         </Row>
       </Container>
     </div>
@@ -52,3 +60,25 @@ function Hero() {
 }
 
 export default Hero;
+
+interface ItemProps {
+  char: string;
+}
+
+const Item = ({ char }: ItemProps) => {
+  const [active, setActive] = useState(false);
+
+  return (
+    <span
+      className={active ? 'animated' : ''}
+      onMouseEnter={() => setActive(true)}
+      onMouseLeave={() =>
+        setTimeout(() => {
+          setActive(false);
+        }, 1000)
+      }
+    >
+      {char}
+    </span>
+  );
+};
