e8da91d9577c41510254959259f8e43c46355661
Jul-Me <memai.julian@gmail.com>
2022-02-09 22:02:15 +0000

    refactor: Removed service component for now

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Nav from './components/Nav/Nav';
 import Hero from './components/Hero/Hero';
 import AboutMe from './components/AboutMe/AboutMe';
-import Service from './components/Service/Service';
 import Portfolio from './components/Portfolio/Portfolio';
 import './App.scss';
 
@@ -12,7 +11,6 @@ function App(): JSX.Element {
       <Nav />
       <Hero />
       <AboutMe />
-      <Service />
       <Portfolio />
     </div>
   );
